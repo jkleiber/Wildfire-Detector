da5cd12e4d14564329df42a6fcb05942f3155896
jkleiber <jkleiber8@gmail.com>
2019-09-13 16:40:59 -0500

    Adds map to the fire map display

--- Smokey/app/src/main/java/com/wildfiredetector/smokey/ui/main/FireMapFragment.kt
+++ Smokey/app/src/main/java/com/wildfiredetector/smokey/ui/main/FireMapFragment.kt
@@ -1,12 +1,17 @@
 package com.wildfiredetector.smokey.ui.main
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.wildfiredetector.smokey.R
+import kotlinx.android.synthetic.main.fragment_fire_map.*
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+
 
 class FireMapFragment : Fragment() {
 
@@ -14,9 +19,14 @@ class FireMapFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         pageViewModel = ViewModelProviders.of(this).get(PageViewModel::class.java).apply {
             setIndex(arguments?.getInt(SectionsPagerAdapter.ARG_SECTION_NUMBER) ?: 1)
         }
+
+        // Load the OSM Droid context
+        val ctx = context
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx))
     }
 
     override fun onCreateView(inflater: LayoutInflater,
@@ -25,8 +35,21 @@ class FireMapFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_fire_map, container, false)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?)
+    {
+        fireMap.setTileSource(TileSourceFactory.MAPNIK)
+    }
 
+    override fun onResume() {
+        super.onResume()
+
+        fireMap.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        fireMap.onPause()
     }
 
 }
