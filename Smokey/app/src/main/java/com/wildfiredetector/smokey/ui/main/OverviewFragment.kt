c6d52ee6cb5a761ab29462d313fba0722f0e8437
jkleiber <jkleiber8@gmail.com>
2019-10-07 18:08:18 -0500

    Pulls fires from database and adds them to the map

--- Smokey/app/src/main/java/com/wildfiredetector/smokey/ui/main/OverviewFragment.kt
+++ Smokey/app/src/main/java/com/wildfiredetector/smokey/ui/main/OverviewFragment.kt
@@ -5,7 +5,6 @@ import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
-import android.util.Log
 import android.util.Log.d
 import android.util.Log.e
 import android.view.LayoutInflater
@@ -16,18 +15,13 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
 import com.google.android.material.snackbar.Snackbar
-import com.wildfiredetector.smokey.Fire
 import com.wildfiredetector.smokey.FireManager
 import com.wildfiredetector.smokey.R
 import com.wildfiredetector.smokey.VolleySingleton
-import kotlinx.android.synthetic.main.fragment_fire_map.*
 import kotlinx.android.synthetic.main.fragment_overview.*
 import org.json.JSONObject
-import java.lang.ClassCastException
 import java.lang.Exception
 
 class OverviewFragment : Fragment() {
@@ -77,12 +71,15 @@ class OverviewFragment : Fragment() {
 
             // Build a new request
             val request = JsonObjectRequest(Request.Method.POST, reportURL, jsonPkt,
-                Response.Listener{
+                com.android.volley.Response.Listener{
                     d("RESPONSE", it.toString())
                     Snackbar.make(view, "Fire reported!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show()
+
+                    // Update the map
+                    pageViewModel.updateMap(true)
                 },
-                Response.ErrorListener {
+                com.android.volley.Response.ErrorListener {
                     e("RESPONSE", it?.message)
                     val errorText = "Failed to report fire: %s".format(it.message)
                     Snackbar.make(view, errorText, Snackbar.LENGTH_LONG)
@@ -92,12 +89,6 @@ class OverviewFragment : Fragment() {
 
             // Add the fire to the database by sending a request using Volley
             VolleySingleton.getInstance(activity!!.applicationContext).addToRequestQueue(request)
-
-            // Add the fire to the map
-            FireManager.addFire(context, currentLocation?.latitude, currentLocation?.longitude)
-
-            // Update the map
-            pageViewModel.updateMap(true)
         }
     }
 
