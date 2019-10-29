e07fb492143d6c620d25326092cde6c9df6c0ec9
jkleiber <jkleiber8@gmail.com>
2019-10-28 22:43:54 -0500

    Adds active fire polygon and restores icon changes that got lost'

--- SmokeyWeb/php/get_active_fires.php
+++ SmokeyWeb/php/get_active_fires.php
@@ -1,11 +1,11 @@
 <?php
-function getAllFires()
+function getActiveFireIDs()
 {
     // Connect to the database
-    require_once("db_connect.php");
+    require("db_connect.php");
 
     // Get list of active fires
-    $query = "SELECT * FROM active_fires";
+    $query = "SELECT id FROM active_fires";
 
     // Attempt to get the list of fires
     try
@@ -24,11 +24,60 @@ function getAllFires()
 
     return $active_fires;
 }
+
+
+function getActiveFireReports($id)
+{
+    // Connect to the database
+    require("db_connect.php");
+
+    // Form a query to get all the fire reports
+    $query = "SELECT * FROM fire_reports WHERE active_fire_id = " . $pdo->quote($id);
+
+    // Attempt to get the reports
+    try
+    {
+        $reports = $pdo->query($query);
+    }
+    // Couldn't get the reports, so print an error
+    catch(PDOException $e)
+    {
+        // Print error
+        printf("%s\n", $e->getMessage());
+
+        // Exit
+        exit();
+    }
+
+    return $reports;
+}
 ?>
 
 <?php
-    // Get all the active fires
-    $fires = getAllFires();
+    // Convex hull library
+    require_once("convex_hull/convexHull.php");
 
-    // TODO: parse to JSON
+    // Get all the active fires
+    $fires = getActiveFireIDs();
+
+    $fire_boundaries = [];
+
+    // Find the convex hull for each active fire
+    while($fire = $fires->fetch())
+    {
+        // Get the fires associated with this one
+        $reports = getActiveFireReports($fire['id']);
+
+        $report_pts = [];
+
+        // Form these reports into a list of points
+        while ($report = $reports->fetch())
+        {
+            $report_pts[] = [$report['latitude'], $report['longitude']];
+        }
+
+        $fire_boundaries[] = json_encode(array("id"=>$fire['id'], "points"=>convexHull($report_pts)));;
+    }
+
+    echo json_encode($fire_boundaries);
 ?>
