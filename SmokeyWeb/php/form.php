273630408ccbb0fd335554c3f959854fdb102064
Pouya Tavakoli <1pouya.tavakoli@gmail.com>
2019-12-01 15:46:58 -0600

    Added a link to the bottom of the map to an html form, then when hitting submit redirects you to a php page that is an 'echo'

---
+++ SmokeyWeb/php/form.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Web</title>
+	</head>
+	<body>
+	<?php
+	
+	$fullName = $_POST['fullName'];
+	$phoneNumber = $_POST['phoneNumber'];
+	
+	echo $fullName, " ", $phoneNumber;
+	
+	?>
+	</body>
+</html>
